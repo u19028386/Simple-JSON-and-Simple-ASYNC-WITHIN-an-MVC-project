baf31bda0c749216267ef9d6b8721464276b7896
Sinenhlanhla Dlamini <u19028386@up.ac.za>
2022-10-30 21:08:14 +0200

    6th change

--- u19028386_HW06/Controllers/HomeController.cs
+++ u19028386_HW06/Controllers/HomeController.cs
@@ -8,23 +8,7 @@ namespace u19028386_HW06.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
+       
     }
 }
--- u19028386_HW06/Views/order_itemsController.cs
+++ u19028386_HW06/Views/order_itemsController.cs
@@ -7,128 +7,129 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using u19028386_HW06.Models;
+
 using PagedList.Mvc;
 using PagedList;
 
 namespace u19028386_HW06.Views
 {
-    public class productsController : Controller
+    public class order_itemsController : Controller
     {
         private BikeStoresEntities db = new BikeStoresEntities();
 
-        public ActionResult Search(string searchText)
+        public ActionResult SearchD(DateTime searchDate)
         {
-            var sSearch = db.products.Where(zz => zz.product_name == searchText).ToList();
-           
-            return View("Index",sSearch);
+            var sSearch = db.order_items.Where(zz => zz.order.order_date == searchDate).ToList();
+
+            return View("Index", sSearch);
         }
 
-        // GET: products
+        // GET: order_items
         public ActionResult Index()
         {
-            var products = db.products.Include(p => p.brand).Include(p => p.category);
-            return View(products.ToList().ToPagedList(1,10));
+            var order_items = db.order_items.Include(o => o.product).Include(o => o.order);
+            return View(order_items.ToList());
         }
 
-        // GET: products/Details/5
+        // GET: order_items/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            product product = db.products.Find(id);
-            if (product == null)
+            order_items order_items = db.order_items.Find(id);
+            if (order_items == null)
             {
                 return HttpNotFound();
             }
-            return View(product);
+            return View(order_items);
         }
 
-        // GET: products/Create
+        // GET: order_items/Create
         public ActionResult Create()
         {
-            ViewBag.brand_id = new SelectList(db.brands, "brand_id", "brand_name");
-            ViewBag.category_id = new SelectList(db.categories, "category_id", "category_name");
+            ViewBag.product_id = new SelectList(db.products, "product_id", "product_name");
+            ViewBag.order_id = new SelectList(db.orders, "order_id", "order_id");
             return View();
         }
 
-        // POST: products/Create
+        // POST: order_items/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "product_id,product_name,brand_id,category_id,model_year,list_price")] product product)
+        public ActionResult Create([Bind(Include = "order_id,item_id,product_id,quantity,list_price,discount")] order_items order_items)
         {
             if (ModelState.IsValid)
             {
-                db.products.Add(product);
+                db.order_items.Add(order_items);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.brand_id = new SelectList(db.brands, "brand_id", "brand_name", product.brand_id);
-            ViewBag.category_id = new SelectList(db.categories, "category_id", "category_name", product.category_id);
-            return View(product);
+            ViewBag.product_id = new SelectList(db.products, "product_id", "product_name", order_items.product_id);
+            ViewBag.order_id = new SelectList(db.orders, "order_id", "order_id", order_items.order_id);
+            return View(order_items);
         }
 
-        // GET: products/Edit/5
+        // GET: order_items/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            product product = db.products.Find(id);
-            if (product == null)
+            order_items order_items = db.order_items.Find(id);
+            if (order_items == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.brand_id = new SelectList(db.brands, "brand_id", "brand_name", product.brand_id);
-            ViewBag.category_id = new SelectList(db.categories, "category_id", "category_name", product.category_id);
-            return View(product);
+            ViewBag.product_id = new SelectList(db.products, "product_id", "product_name", order_items.product_id);
+            ViewBag.order_id = new SelectList(db.orders, "order_id", "order_id", order_items.order_id);
+            return View(order_items);
         }
 
-        // POST: products/Edit/5
+        // POST: order_items/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "product_id,product_name,brand_id,category_id,model_year,list_price")] product product)
+        public ActionResult Edit([Bind(Include = "order_id,item_id,product_id,quantity,list_price,discount")] order_items order_items)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(product).State = EntityState.Modified;
+                db.Entry(order_items).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.brand_id = new SelectList(db.brands, "brand_id", "brand_name", product.brand_id);
-            ViewBag.category_id = new SelectList(db.categories, "category_id", "category_name", product.category_id);
-            return View(product);
+            ViewBag.product_id = new SelectList(db.products, "product_id", "product_name", order_items.product_id);
+            ViewBag.order_id = new SelectList(db.orders, "order_id", "order_id", order_items.order_id);
+            return View(order_items);
         }
 
-        // GET: products/Delete/5
+        // GET: order_items/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            product product = db.products.Find(id);
-            if (product == null)
+            order_items order_items = db.order_items.Find(id);
+            if (order_items == null)
             {
                 return HttpNotFound();
             }
-            return View(product);
+            return View(order_items);
         }
 
-        // POST: products/Delete/5
+        // POST: order_items/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            product product = db.products.Find(id);
-            db.products.Remove(product);
+            order_items order_items = db.order_items.Find(id);
+            db.order_items.Remove(order_items);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
